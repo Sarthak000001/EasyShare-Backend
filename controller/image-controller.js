4915cee4668c256adc256e5d8d5131d97fd52800
Sarthak Nirgude <100946019+Sarthak000001@users.noreply.github.com>
2024-08-06 22:51:47 +0530

    Update image-controller.js

--- controller/image-controller.js
+++ controller/image-controller.js
@@ -1,17 +1,20 @@
-import path from "path";
 import File from "../models/file.js";
-
+import bcrypt from 'bcrypt';
 
 export const uploadImage = async(request,respone) =>{
-    console.log(request)
+    // console.log(request)
     const fileObj = {
         path: request.file.path,
         name: request.file.originalname
      }
+     if (password) {
+        const saltRounds = 10;
+        fileObj.password = await bcrypt.hash(password, saltRounds);
+    }
     try{
 
        const file = await File.create(fileObj); 
-       respone.status(200).json({path:`http://localhost:8000/file/${file._id}`})
+       respone.status(200).json({path:`https://easyshare-backend-0d13.onrender.com/file/${file._id}`})
     }
     catch(err){
         console.log(err.message);
@@ -19,18 +22,50 @@ export const uploadImage = async(request,respone) =>{
     }
 }
 
-export const downloadImage = async(request,response) =>{
-    try{
-        const file = await File.findById(request.params.fileId)
+export const downloadImage = async (request, response) => {
+    try {
+        const file = await File.findById(request.params.fileId);
+
+        if (!file) {
+            return response.status(404).json({ error: 'File not found' });
+        }
+
+        const password = request.query.password; 
+
+        if (file.password) {
+            if (!password) {
+                return response.status(400).json({ error: 'Password required' });
+            }
+
+            const isMatch = await bcrypt.compare(password, file.password);
+            console.log("isMatch : ",isMatch);
+            if (!isMatch) {
+                return response.status(403).json({ error: 'Invalid password' });
+            }
+        }
 
         file.dowloadContent++;
-
         await file.save();
-
-        response.download(file.path,file.name);
-    }
-    catch(err){
+        console.log("File ===> ",file)
+        const fileUrl = `https://easyshare-backend-0d13.onrender.com/file/download/${file._id}`;
+        response.status(200).json({ fileUrl });
+   
+    } catch (err) {
         console.error(err.message);
-        return response.status(500).json({err:err.message});
+        response.status(500).json({ error: err.message });
     }
 }
+
+// New route for serving file download
+export const serveDownload = async (request, response) => {
+    try {
+        const file = await File.findById(request.params.fileId);
+        if (!file) {
+            return response.status(404).send('File not found');
+        }
+        response.download(file.path, file.name);
+    } catch (err) {
+        console.error(err.message);
+        response.status(500).send('Server Error');
+    }
+};
